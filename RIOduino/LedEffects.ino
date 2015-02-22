0fb956e72f329a5057332a84a4f38b7f611cdeee
Jake Powell <jakedp7@gmail.com>
2015-02-22 17:18:41 -0500

    Added colorSpectrum function code, copied from NeoPixel library with a few changes

--- RIOduino/LedEffects.ino
+++ RIOduino/LedEffects.ino
@@ -24,8 +24,23 @@ void setStrip(Adafruit_NeoPixel &pixelStrip, int intensity, uint32_t color) {
 }
 
 /*  Takes a value from 0 to 1 and returns a color corresponding
-*   to the input value's position along a colorspectrum */
-uint32_t colorSpectrum(float proportion);
+*   to the input value's position along a color spectrum. */
+uint32_t colorSpectrum(float proportion) {
+  
+  //Convert the proportion into a byte, between 0-255
+  byte WheelPosition = proportion * 255;
+  //Use the position to create and return color values based on position
+  WheelPosition = 255 - WheelPosition;
+  if(WheelPosition < 85) {
+    return Adafruit_NeoPixel::Color(255 - WheelPosition * 3, 0, WheelPosition * 3);
+  } else if(WheelPosition < 170) {
+    WheelPosition = WheelPosition - 85;
+    return Adafruit_NeoPixel::Color(0, WheelPosition * 3, 255 - WheelPosition * 3);
+  } else {
+    WheelPosition = WheelPosition - 170;
+    return Adafruit_NeoPixel::Color(WheelPosition * 3, 255 - WheelPosition * 3, 0);
+  }
+}
 
 /*  Takes a linear proportion from 0 to 1 and returns a corresponding 
 *   value between 0 and 1, related to the input's placement on a sine wave. 
@@ -39,4 +54,4 @@ float mapToWave(float proportion) {
   float pi = 3.14159;
   float mappedProportion = (0.5 * sin((2 * pi * proportion) - (0.5 * pi))) + 0.5;
   return mappedProportion;
-};
+}
